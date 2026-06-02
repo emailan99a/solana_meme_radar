207b147c2e6fa010853b1bb1ffc6f22afc8cb648
emailan99a <email.an99a@gmail.com>
2026-06-02 21:31:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 PORT = int(os.environ.get("PORT", 8765))
 HOST = "0.0.0.0"
 
-# Radar Filters
+# Search
 SEARCH_TERMS = [
     "dog",
     "cat",
@@ -15,12 +15,20 @@ SEARCH_TERMS = [
     "sol",
     "pump",
     "moon",
-    "frog"
+    "frog",
 ]
 
+# Filters
 MIN_AGE_DAYS = 7
-MIN_LIQUIDITY_USD = 25000
-MIN_VOLUME_24H_USD = 5000
+MAX_AGE_DAYS = 3650
+
+MIN_LIQUIDITY_USD = 25_000
+
+MIN_MARKET_CAP_USD = 100_000
+MAX_MARKET_CAP_USD = 50_000_000
+
+MIN_VOLUME_24H_USD = 5_000
+
 MIN_TXNS_24H = 20
 
 TOP_N = 10
